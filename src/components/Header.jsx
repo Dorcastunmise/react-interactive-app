97da461d4791478d501738d993511b7ce1a62bbf
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-08-18 17:02:20 +0100

    structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./src/components/meme.jpg" 
+                src="./src/meme.jpg" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
